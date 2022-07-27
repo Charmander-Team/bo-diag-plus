48f2d02afe88f5d9a8261268276d65525c6d6b65
Kris Bang <dc.ambang@ecole-ipssi.net>
2022-07-27 11:38:23 +0200

    Add new axios && private route

--- src/contexts/index.jsx
+++ src/contexts/index.jsx
@@ -1,19 +1,72 @@
-import { createContext, useState } from "react";
+import { useRouter } from "next/router";
+import { createContext, useState, useEffect } from "react";
+import Authentication from "../services/Authentication";
 
 const MainContext = createContext({});
 
 const Provider = ({ children }) => {
+  const router = useRouter();
   //   Process Connexion route privée
   const [login, setLogin] = useState(null);
   const connectedAdmin = async (event, opts) => {
     event.preventDefault();
     console.log("e", event);
     console.log("opts", opts);
-    setLogin(true);
+
+    const token = await Authentication.byPostToken(opts);
+
+    console.log("token", token);
+    console.log("router", router);
+    if (token) {
+      router.push("/");
+      setLogin(true);
+      localStorage.setItem("tokenRefresh", token.refresh);
+      localStorage.setItem("tokenAccess", token.access);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("tokenRefresh")) {
+      const token = localStorage.getItem("tokenRefresh");
+      const refresh = async () => {
+        const response = await Authentication.refresh(token);
+        console.log("response", response);
+        if (response.code != "token_not_valid") {
+          localStorage.setItem("tokenAccess", response.access);
+          setLogin(true);
+        } else {
+          setLogin(false);
+        }
+      };
+
+      refresh();
+    }
+
+    console.log("router auth", router);
+    if (
+      !login &&
+      router.pathname != "/authentification" &&
+      !localStorage.getItem("tokenAccess")
+    ) {
+      router.push("authentification");
+    }
+
+    if (login && router.pathname == "/authentification") {
+      console.log("back");
+      router.push("/");
+    }
+  }, [router, login]);
+
+  // Deconnected function
+  const deconnectedAdmin = async (event) => {
+    event.preventDefault();
+    setLogin(null);
+    localStorage.removeItem("tokenRefresh");
+    localStorage.removeItem("tokenAccess");
   };
 
   return (
-    <MainContext.Provider value={{ connectedAdmin, login }}>
+    <MainContext.Provider value={{ connectedAdmin, login, deconnectedAdmin }}>
       {children}
     </MainContext.Provider>
   );
