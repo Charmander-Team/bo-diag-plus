b7edd5ff09e7d24699ceb1026bdebd4db5cb98cc
Kris Bang <dc.ambang@ecole-ipssi.net>
2022-07-27 11:53:53 +0200

    Add feature deconnexion

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,42 +1,56 @@
-import styles from '../../styles/Navigation.module.scss'
+import styles from "../../styles/Navigation.module.scss";
+import MainContext from "../contexts";
+import { useContext } from "react";
 
-const Navigation = ()=>{
-    return (
-        <div className={styles.navigation}>
-            <ul>
-                <li>
-                    <a href='#'>
-                        <span className={styles.item}>Logo</span>
-                    </a>
-                </li>
-                <li>
-                    <a href='#'>
-                        <span className={styles.item}>Accueil</span>
-                    </a>
-                </li>
-                <li>
-                    <a href='#'>
-                        <span className={styles.item}>Patients</span>
-                    </a>
-                </li>
-                <li>
-                    <a href='#'>
-                        <span className={styles.item}>Praticiens</span>
-                    </a>
-                </li>
-                <li>
-                    <a href='#'>
-                        <span className={styles.item}>Articles</span>
-                    </a>
-                </li>
-                <li>
-                    <a href='#'>
-                        <span className={styles.item}>Diagnostiques</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    )
-}
+const Navigation = () => {
+  const { deconnectedAdmin } = useContext(MainContext);
 
-export default Navigation
+  return (
+    <div className={styles.navigation}>
+      <ul>
+        <li>
+          <a href="#">
+            <span className={styles.item}>Logo</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className={styles.item}>Accueil</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className={styles.item}>Patients</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className={styles.item}>Praticiens</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className={styles.item}>Articles</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className={styles.item}>Diagnostiques</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={(event) => {
+              deconnectedAdmin(event);
+            }}
+          >
+            <span className={styles.item}>Déconnexion</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
